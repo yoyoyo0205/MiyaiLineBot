73596e21f2c7d8b9fbd463b349f4d30c1c7c694e
yo_nakazawa <y.n_green@hotmail.co.jp>
2021-04-27 23:36:12 +0900

    first commit

---
+++ .aws-sam/build/EndPointFunction/mylinebot.py
@@ -0,0 +1,38 @@
+"""
+オウム返し Line Bot
+"""
+
+import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+
+
+def lambda_handler(event, context):
+    headers = event["headers"]
+    body = event["body"]
+
+    # get X-Line-Signature header value
+    signature = headers['x-line-signature']
+
+    # handle webhook body
+    handler.handle(body, signature)
+
+    return {"statusCode": 200, "body": "OK"}
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    """ TextMessage handler """
+    input_text = event.message.text
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=input_text+'だみっ'))
