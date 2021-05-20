d2180a0c6f9e278b44b26e2b3524788bbab2384d
yo_nakazawa <y.n_green@hotmail.co.jp>
2021-05-20 23:37:05 +0900

    response取得完成

--- src/mylinebot.py
+++ src/mylinebot.py
@@ -8,12 +8,15 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage,
 )
+
+import boto3
 
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 
+client = boto3.client('rekognition')
 
 def lambda_handler(event, context):
     headers = event["headers"]
@@ -36,3 +39,28 @@ def handle_text_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=input_text+'だみっ'))
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+    # ユーザから送られてきた画像を一時ファイルとして保存
+    message_content = line_bot_api.get_message_content(event.message.id)
+    file_path = "/tmp/send-image.jpg"
+    with open(file_path, 'wb') as fd:
+        for chunk in message_content.iter_content():
+            fd.write(chunk)
+
+    # Rekognition で感情分析する
+    with open(file_path, 'rb') as fd:
+        send_image_binary = fd.read()
+        response = client.detect_faces(Image={"Bytes":send_image_binary},
+                                      Attributes=["ALL"])
+    print(response)
+
+
+    # 返答を送信する
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=str(response)[:1000]))
+
+    # file_pathの画像を削除
+    os.remove(file_path)
